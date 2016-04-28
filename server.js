33264cd6f6cbdc70e06f07bdf81b1cf3980d4937
Bradley Stachurski <bradley.stachurski@gmail.com>
2016-04-28 17:30:10 -0500

    First commit

---
+++ server.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var fs = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+var app = express();
+
+app.get('/scrape', function(req, res) {
+
+    url = 'http://www.imdb.com/title/tt1229340/';
+
+    request(url, function(error, response, html) {
+        if(!error) {
+            var $ = cheerio.load(html);
+
+            var title, release, rating;
+            var json = { title: "", release : "", rating : ""}
+
+
+            $('h1[itemprop="name"]').filter(function(){
+                var data = $(this);
+                title = data.text();
+                json.title = title;
+            });
+
+            $('#titleYear').filter(function(){
+                var data = $(this);
+                release = data.text();
+                json.release = release;
+            });
+
+            $('span[itemprop="ratingValue"]').filter(function(){
+                var data = $(this);
+                rating = data.text();
+                json.rating = rating;
+            });
+
+
+
+        }
+
+        fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err) {
+            console.log('File successfully written! - Check your project directory for the output.json file');
+        });
+
+        res.send('Check your console!')
+
+    })
+
+});
+
+app.listen('8081');
+
+console.log('Magic happens on port 8081');
+
+exports = module.exports = app;
